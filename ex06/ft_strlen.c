a584b749717a9a32c29dcc6223b74c0d0cefa980
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-01-02 18:17:27 +0100

    Add ex06

---
+++ ex06/ft_strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	main(void)
+{
+	char	*str;
+
+	str = "Hello, World!";
+	printf("ft_strlen(\"%s\") = %d;\n", str, ft_strlen(str));
+}
