e9ed682917bd499d97d200df338e5e633a9abf0b
3ka1tz <ekaitzlopez2025@gmail.com>
2026-01-07 12:06:08 +0100

    Implement main()

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void	ft_swap(int *a, int *b)
 {
 	int	tmp;
@@ -5,4 +7,19 @@ void	ft_swap(int *a, int *b)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
+}
+
+int	main(void)
+{
+	int	n0;
+	int	n1;
+	int	*a;
+	int	*b;
+
+	n0 = 0;
+	n1 = 1;
+	a = &n0;
+	b = &n1;
+	ft_swap(a, b);
+	printf("*a = %d, *b = %d\n", *a, *b);
 }
