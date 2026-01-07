371aca54ac459fa391f1af081984320d564d15f6
3ka1tz <ekaitzlopez2025@gmail.com>
2026-01-07 12:15:56 +0100

    Implement main()

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
+
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	*div = a / b;
 	*mod = a % b;
+}
+
+int	main(void)
+{
+	int	div;
+	int	mod;
+
+	ft_div_mod(5, 2, &div, &mod);
+	printf("*div = %d, *mod = %d\n", div, mod);
 }
