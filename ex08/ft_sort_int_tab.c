ece6239a3b3d0e1cfa11d04f56729902a9a7d5b2
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-01-07 18:25:17 +0100

    Add ex08

---
+++ ex08/ft_sort_int_tab.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	tmp;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			++i;
+	}
+}
+
+int	main(void)
+{
+	int	tab[5];
+	int	size;
+	int	i;
+
+	tab[0] = 4;
+	tab[1] = 3;
+	tab[2] = 2;
+	tab[3] = 1;
+	tab[4] = 0;
+	size = 5;
+	ft_sort_int_tab(tab, size);
+	i = 0;
+	while (i < size)
+	{
+		printf("tab[%d] = %d\n", i, tab[i]);
+		++i;
+	}
+}
